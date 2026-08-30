ad397d738cf4c47856e387e7a54b6711340c00a2
Vidya0904 <vidyajaywant2000@gmail.com>
2026-08-30 14:08:27 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>Cart Project Started</h1>
+      <Button variant="contained">Click Me</Button>
+    </>
+  )
+}
+
+export default App
