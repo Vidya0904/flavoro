3cb8074ce40ea3e9429b700aba2e7bfdf69c4779
Vidya0904 <vidyajaywant2000@gmail.com>
2026-09-01 11:52:10 +0530

    connectin string

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("YOUR_MONGO_URL");
+    // await mongoose.connect("mongodb+srv://vidyajaywant2000_db_user:vidya123@cluster0.bkxexjm.mongodb.net/?appName=Cluster0");
+    await mongoose.connect("mongodb+srv://vidyajaywant2000_db_user:vidya123@cluster0.bkxexjm.mongodb.net/cartDB?retryWrites=true&w=majority");
     console.log("MongoDB Connected");
+
+    console.log("Database:", mongoose.connection.name);
   } catch (error) {
     console.log(error);
   }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
+const Products = require("./models/Products");
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
+});
+
+
+app.get("/products", async (req, res) => {
+  const products = await Products.find();
+  res.json(products);
 });
 
 app.listen(5000, () => {
